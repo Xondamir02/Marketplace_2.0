e9c4d8ac06d42eae583048f3c1dae309102f97b6
Xondamir <xondamiraxmadjonov2002@gmail.com>
2023-06-12 21:28:44 +0500

    Updated

---
+++ Marketplace.Services.Products/Marketplace.Services.Products/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace Marketplace.Services.Products.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Categoryid { get; set; }
+        public Category Category { get; set; }
+        public List<ProductImage>? Images { get; set; }
+    }
+
+    public class ProductImage
+    {
+        public Guid Id { get;set; }
+        public string ProductId { get; set; }
+        public required string Path { get; set; }
+    }
+}
